d6fef2e152873f41674a5873be46e3d832612ddb
marabesi <matheus.marabesi@gmail.com>
2019-12-29 21:29:16 +0100

    created js version of the front end

---
+++ front-end-js/src/relayEnvironment.js
@@ -0,0 +1,33 @@
+import {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} from 'relay-runtime';
+
+const GRAPHQL_HOST = process.env.GRAPHQL_HOST;
+
+function fetchQuery(
+  operation,
+  variables,
+) {
+  return fetch(GRAPHQL_HOST, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json();
+  });
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
